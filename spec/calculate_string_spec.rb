610d112780048c88c5b09925a57109dc215be916
kanchankotiya2017 <kanchankotiya2017@gmail.com>
2025-05-10 16:58:27 +0530

    Added test case for "1", "1,2"

--- spec/calculate_string_spec.rb
+++ spec/calculate_string_spec.rb
@@ -8,4 +8,16 @@ describe CalculatString do
       expect(calculate_string.add("")).to eq(0)
     end
   end
+
+  it "takes 1 number" do
+    expect(calculate_string.add("1")).to eq(1)
+  end
+
+  it "takes 2 numbers" do
+    expect(calculate_string.add("1,7")).to eq(8)
+  end
+
+  it "handles an n amount of numbers" do
+    expect(calculate_string.add("1,2,3,4,5,6")).to eq(21)
+  end
 end
